1f3964f82d7a6055c9d2a10381920e921a6eb8c1
Anatolii Pyroh <tolikpirog7@gmail.com>
2022-08-23 18:18:31 +0300

    add city info above form on submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import { Container, Box } from "@mui/material";
-
+import { Container, Box, TextField, Button } from "@mui/material";
 
 function App() {
   const [weatherData, setWeatherData] = useState();
+  const [inputValue, setInputValue] = useState("");
 
-  const getApi = async () => {
-    const request = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Киев&units=metric&appid=${import.meta.env.VITE_WEATHER_API_KEY}`)
-    const response = request.data
-    console.log(response)
+  const getApi = async (e) => {
+    e.preventDefault();
+    if (inputValue.trim().length > 3) {
+      const request = await axios.get(
+        `${import.meta.env.VITE_BASIC_URL}?q=${inputValue}&units=metric&appid=${
+          import.meta.env.VITE_WEATHER_API_KEY
+        }`
+      );
+      setInputValue("");
+      const response = request.data;
+      console.log(response);
+      setWeatherData(response);
+
+    }
   };
 
-  useEffect(() => {
-    getApi()
-  }, [])
   return (
-    <div className='App'>
-      <Container maxWidth='xl'>
-        <Box>123</Box>
-      </Container>
-    </div>
+    <Container maxWidth='xl'>
+      <Box className='App'>
+        <h1>{weatherData?.name},{weatherData?.sys?.country}</h1>
+        <Box>
+          <form autoComplete='off' onSubmit={getApi}>
+            <TextField
+              label='enter city name'
+              variant='outlined'
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              fullWidth
+            />
+            <Button type='submit' variant='contained' sx={{ mt: 2 }} fullWidth>
+              Find
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Container>
   );
 }
 
