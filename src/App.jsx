e7eac5774bcd67ed81e387ba06ee1400d46a4c62
Anatolii Pyroh <tolikpirog7@gmail.com>
2022-08-23 16:41:51 +0300

    add getting city weather info from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
+
 
 function App() {
   const [weatherData, setWeatherData] = useState();
+
+  const getApi = async () => {
+    const request = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Киев&units=metric&appid=${import.meta.env.VITE_WEATHER_API_KEY}`)
+    const response = request.data
+    console.log(response)
+  };
+
+  useEffect(() => {
+    getApi()
+  }, [])
   return (
     <div className='App'>
-      <Container maxWidth='xl'>123</Container>
+      <Container maxWidth='xl'>
+        <Box>123</Box>
+      </Container>
     </div>
   );
 }
