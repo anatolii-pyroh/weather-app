b6af3ceed06d9588b41f59690538b00841c9e8a7
Anatolii Pyroh <tolikpirog7@gmail.com>
2022-08-26 11:27:46 +0300

    add toggle button to switch between daily,forecast

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 import "./App.css";
-import { useEffect, Fragment } from "react";
-import { Container, Box, TextField, Button } from "@mui/material";
+import { useState, useEffect, Fragment } from "react";
+import {
+  Container,
+  Box,
+  Button,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
 import { getApi } from "./api/api";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  addCurrentWeather,
-  saveCity,
-} from "./redux/reducers/currentWeatherSlice";
+import { addCurrentWeather } from "./redux/reducers/currentWeatherSlice";
 import CitiesAutoComplete from "./components/CitiesAutoComplete/CitiesAutoComplete";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 
 function App() {
+  const [alignment, setAlignment] = useState("daily");
   const weather = useSelector((state) => state.currentWeather.info);
   const dispatch = useDispatch();
+
+  // switch selected button content
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
   // receiving data from input (geoDB cities API) and send it to getApi function,
-  // also giving our state from redux a new value(city info)
+  // also giving our state from redux a new value(current city info)
   const handleOnSearchChange = async (cityData) => {
     const response = await getApi(cityData.value);
     console.log(response);
-    dispatch(addCurrentWeather(response));
+    dispatch(addCurrentWeather(response.current));
   };
 
   useEffect(() => {
@@ -31,20 +40,21 @@ function App() {
         <CitiesAutoComplete onSearchChange={handleOnSearchChange} />
         {weather && (
           <Fragment>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Box sx={{ display: "flex", flexDirection: "column" }}>
-                <h2>
-                  {weather?.current?.name} , {weather?.current?.sys?.country}
-                </h2>
-                <Button
-                  variant='contained'
-                  onClick={() => dispatch(saveCity(weather))}
-                >
-                  Save city
-                </Button>
-              </Box>
+            {/* daily,forecast,saved cities buttons */}
+            <Box sx={{ display: "flex", justifyContent: "center", mt: 3 }}>
+              <ToggleButtonGroup
+                color='primary'
+                value={alignment}
+                exclusive
+                onChange={handleChange}
+                aria-label='Platform'
+              >
+                <ToggleButton value='daily'>Daily</ToggleButton>
+                <ToggleButton value='forecast'>Forecast</ToggleButton>
+                <ToggleButton value='saved cities'>Saved cities</ToggleButton>
+              </ToggleButtonGroup>
             </Box>
-            <CurrentWeather />
+            {alignment === "daily" && <CurrentWeather />}
           </Fragment>
         )}
       </div>
--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { Container, Box, TextField, Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
+import { saveCity } from "../../redux/reducers/currentWeatherSlice";
 
 const CurrentWeather = () => {
   const dispatch = useDispatch();
   const currentWeather = useSelector((state) => state.currentWeather.info);
   console.log(currentWeather);
 
+  const time = new Date(currentWeather.dt * 1000).toLocaleTimeString("it-IT");
+  const date = new Date(currentWeather.dt * 1000).toLocaleDateString("en-GB");
   const dayNumber = new Date().getDay();
-  let today;
-  if (dayNumber === 1) today = "Monday";
-  if (dayNumber === 2) today = "Saturday";
-  if (dayNumber === 3) today = "Wednesday";
-  if (dayNumber === 4) today = "Thursday";
-  if (dayNumber === 5) today = "Friday";
-  if (dayNumber === 6) today = "Saturday";
-  if (dayNumber === 7) today = "Sunday";
+  let weekDay;
+  if (dayNumber === 1) weekDay = "Monday";
+  if (dayNumber === 2) weekDay = "Saturday";
+  if (dayNumber === 3) weekDay = "Wednesday";
+  if (dayNumber === 4) weekDay = "Thursday";
+  if (dayNumber === 5) weekDay = "Friday";
+  if (dayNumber === 6) weekDay = "Saturday";
+  if (dayNumber === 7) weekDay = "Sunday";
   return (
     <Box
       sx={{
@@ -24,14 +27,32 @@ const CurrentWeather = () => {
         flexDirection: "column",
         padding: "0 1rem",
         borderRadius: "20px",
-        mt: 5,
+        mt: 3,
       }}
     >
-        
-      <h3 style={{alignSelf: "center"}}>
-        {today}, {new Date().toLocaleDateString()}
-      </h3>
-
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <h4>
+            {currentWeather?.name}, {currentWeather?.sys?.country}
+          </h4>
+          <Button
+            variant='outlined'
+            size='small'
+            sx={{ ml: 2 }}
+            onClick={() => dispatch(saveCity(weather))}
+          >
+            Save city
+          </Button>
+        </Box>
+        <h4 style={{ alignSelf: "start" }}>
+          {weekDay}, {date}, {time}
+        </h4>
+      </Box>
     </Box>
   );
 };
